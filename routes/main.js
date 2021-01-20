3466e7b7cebe0a9fb9c40490691fb86a6389c6bd
Yaron Cohen <yaroncbtv@gmail.com>
2021-01-20 12:26:07 +0200

    add HTML

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = require("express").Router();
+const path = require('path');
 
-
-router.get("/", async (req, res) => {
-    res.json(
-      'Home'
-    );
-  });
-
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 module.exports = router;
