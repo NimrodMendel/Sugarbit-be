284e9dcd288c64790b2bb63fe78c4771d01feed2
ido <idomand@gmail.com>
2021-01-20 17:34:13 +0200

    adding body parser

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 //const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-
+const bodyParser = require("body-parser");
 // set up express
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
